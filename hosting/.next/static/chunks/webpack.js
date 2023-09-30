31556a6dc7280607e3bad01e3906eadd1d6d782f
Honeybadger <rajimoshood200482@gmail.com>
2023-09-30 20:09:53 +0100

    updated css to work fine

--- hosting/.next/static/chunks/webpack.js
+++ hosting/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cabc565afadfeab3"; }
+/******/ 		__webpack_require__.h = function() { return "5ab18a8d2ed25d3e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
